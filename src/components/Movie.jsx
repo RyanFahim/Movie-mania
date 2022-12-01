111947db9a73e84b0b59ae99610e0d4f63fbf83f
RyanFahim <rfahim098@gmail.com>
2022-12-02 01:26:44 +0600

    Thursday update home

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -1,13 +1,32 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom';
 import { useGlobalContext } from './Context'
 
 const Movie = () => {
     const {movie} =useGlobalContext();
+    
+
   return (
     <>
-        {movie.map((currMovie)=>{
-            return <div>{currMovie.Title}</div>
-        })}
+        <div className="section movie-page">
+          <div className="grid grid-3-col container">
+            {movie.map((curMovie)=>{
+              const {imdbID, Title, Poster} = curMovie;
+              const movieName = Title.substring(0, 15);
+              return(
+                <NavLink to={`movie/${imdbID}`}>
+                  <div className="card">
+                    <div className="card-info">
+                      <h2> {movieName.length>=15 ? `${movieName}...`: movieName} </h2>
+                      <img src= {Poster} alt= {imdbID} className= "movie_poster" />
+                    </div>
+                  </div>
+                </NavLink>
+               );
+            })}
+          </div>
+        </div>
+        
     </>
   )
 }
